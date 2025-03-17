0d186081f893217a602022b822cf5416c2630acc
SaraTuset <117302443+SaraTuset@users.noreply.github.com>
2025-03-17 20:51:53 +0100

    Ejercicios listas
    Hechos todos menos el último

---
+++ lista-doble/uListadeListas.pas
@@ -0,0 +1,43 @@
+unit uListadeListas;
+
+interface
+
+uses
+    SysUtils, 
+    uListaEnlazadaDoble;
+
+type
+
+    nodo = record
+        persona: string; // Información almacenada en el nodo
+        lista: tListaDoble; // Información almacenada en el nodo
+        sig: ^nodo; // Puntero al siguiente nodo
+        ant: ^nodo; // Puntero al nodo anterior
+    end;
+
+    tListaDeListas = record
+        first, last: ^nodo; // Punteros al primer y último nodo de la lista
+    end;
+
+    procedure inicializar(var list: tListaDeListas);
+    function hay_facturas(list: tListaDeListas): boolean;
+    function hay_facturas_de_persona(list: tListaDeListas; persona: string): boolean;
+    procedure obtener_facturas_de_persona(list: tListaDeListas; persona: string; var lista: tListaDoble);
+    function obtener_factura_de_persona(list: tListaDeListas; persona: string): integer;
+    procedure pagar_facturas_de_persona(var list: tListaDeListas; persona: string);
+    procedure pagar_factura_de_persona(var list: tListaDeListas; persona: string);
+    procedure agregar_factura_a_persona(var list: tListaDeListas; persona: string; factura: integer);
+    function imprimir_facturas(list: tListaDeListas): string;
+
+implementation
+   procedure inicializar(var list: tListaDeListas);
+   function hay_facturas(list: tListaDeListas): boolean;
+   function hay_facturas_de_persona(list: tListaDeListas; persona: string): boolean;
+   procedure obtener_facturas_de_persona(list: tListaDeListas; persona: string; var lista: tListaDoble);
+   function obtener_factura_de_persona(list: tListaDeListas; persona: string): integer;
+   procedure pagar_facturas_de_persona(var list: tListaDeListas; persona: string);
+   procedure pagar_factura_de_persona(var list: tListaDeListas; persona: string);
+   procedure agregar_factura_a_persona(var list: tListaDeListas; persona: string; factura: integer);
+   function imprimir_facturas(list: tListaDeListas): string;
+
+end.
